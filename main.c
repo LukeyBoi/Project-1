1919834a830835c95e21e283026ebb088efcbda6
Luke Board <dauntingmist34@gmail.com>
2019-04-04 05:27:16 +0000

    Finished work 4/4/19

--- main.c
+++ main.c
@@ -8,7 +8,23 @@ printf("3. Decrypt a message in rotational cipher\n");
 printf("4. Decrypt a message in substitution cipher\n");
 scanf("%d", &i);
  if(i==1){
-     printf("You chose to encrypt with rotational\n");    
+     printf("You chose to encrypt with rotational cipher, would you like to provide a key? Type 1 for no, or 2 for yes.\n");
+           i=0;
+           scanf("%d", &i);
+              if(i==1){
+                  printf("Sorry, I haven't completed this yet. Come back later\n");
+              }
+              else if(i==2){
+                  printf("Please enter a value between 1 and 26\n");
+              }
+      
+     
+     
+     
+     
+     
+     
+     
  }
  else if(i==2){
      printf("You chose to encrypt with sustitution\n");
@@ -19,5 +35,13 @@ scanf("%d", &i);
  else if(i==4){
      printf("You chose to decrypt a substitution\n");
  }
+ 
+ 
+     
+     
+     
+ 
+ 
+ 
 return 0;
 }
