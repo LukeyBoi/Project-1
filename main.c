4ebe5a0f1ba3f7e3a1f36ed8bcbf75c20e1f5d04
Luke Board <dauntingmist34@gmail.com>
2019-04-04 05:12:50 +0000

    Sort out the choosing of the operation

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
-#indlude <stdio.h>
-
+#include <stdio.h>
 int main(){
- printf()
-
-
-
+    int a, b, c, d, i;
+printf("Choose what you would like to do today:\n");
+printf("1. Encrypt a message with the rotational cipher\n");
+printf("2. Encrypt a message with the substitution cipher\n");
+printf("3. Decrypt a message in rotational cipher\n");
+printf("4. Decrypt a message in substitution cipher\n");
+scanf("%d", &i);
+ if(i==1){
+     printf("You chose to encrypt with rotational\n");    
+ }
+ else if(i==2){
+     printf("You chose to encrypt with sustitution\n");
+ }
+ else if(i==3){
+     printf("You chose to decipher a rotation\n");
+ }
+ else if(i==4){
+     printf("You chose to decrypt a substitution\n");
+ }
 return 0;
 }
